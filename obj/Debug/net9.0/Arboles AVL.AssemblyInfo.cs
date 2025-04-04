8925a5af055a91cebc44c1d74a65540e26a55f81
mototur <asandovalp9@miumg.edu.gt>
2025-04-03 18:34:43 -0600

    Tarea Arboles AVL

--- obj/Debug/net9.0/Arboles AVL.AssemblyInfo.cs
+++ obj/Debug/net9.0/Arboles AVL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arboles AVL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc6af53bc12726c6e538470dc92d90e1b71d45f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd02d5bae2c0f9b03d6a58bbe7a2a4c2041fdfe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arboles AVL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arboles AVL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
